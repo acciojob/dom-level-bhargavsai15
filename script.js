103ed8a6ec35affbdd2c2179cf3ba745f970a261
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-23 12:46:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const ulList = document.querySelector('ul').children;
+let level = 0;
+for (let i = 0; i < ulList.length; i++){
+  if (ulList[i].id == 'level') {
+    alert('The level of the element is: ' + level);
+    break;
+  }
+  level++;
+  
+}
